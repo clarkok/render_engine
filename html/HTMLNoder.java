a7a222ffe0f4926d0b51571caa672612b18850e6
Clarkok <mail@clarkok.com>
2014-02-06 14:35:47 +0800

    Add comment in HTMLNoder

--- html/HTMLNoder.java
+++ html/HTMLNoder.java
@@ -14,7 +14,12 @@ public class HTMLNoder {
         this.previous = null;
     }
 
+    /**
+     * Get next node
+     * @return the next node
+     */
     public DOMNode nextNode() throws IOException {
+        // return the previous if exists
         if (this.previous != null) {
             DOMNode ret = this.previous;
             this.previous = null;
@@ -39,6 +44,11 @@ public class HTMLNoder {
         }
     }
 
+    /**
+     * Put back a node to this.previous
+     * @param t the node to put back
+     * @return t if success, else the origin this.previous is replaced and returned
+     */
     public DOMNode backNode(DOMNode t) throws IOException {
         if (this.previous != null) {
             DOMNode ret = this.previous;
@@ -48,22 +58,32 @@ public class HTMLNoder {
         return this.previous = t;
     }
 
+    /**
+     * Peek the next node, without getting it out
+     * @return the next node
+     */
     public DOMNode peekNode() throws IOException {
         DOMNode ret = this.nextNode();
         return this.backNode(ret);
     }
 
+    /**
+     * Parse the following token as a tag, until HT_RIGHT_ANGLE_BRACKET met
+     * @return the parsed node
+     */
     private DOMNode parseTag() throws IOException {
         DOMNode ret = new DOMNode();
         HTMLToken t = this.it.nextToken();  // the '<'
         
-        t = this.it.nextToken();
+        t = this.it.nextToken();            // '/' or tag_name
 
         if (t.getType() == HTMLToken.HTMLTokenType.HT_SLASH) {
+            // '/' met
             ret.type = DOMNode.DOMNodeType.DNT_CLOSE;
             t = this.it.nextToken();
         }
         else {
+            // tag_name met
             ret.type = DOMNode.DOMNodeType.DNT_TAG;
         }
 
@@ -74,9 +94,11 @@ public class HTMLNoder {
             // TODO report parse error
         }
 
+        // loop to parse attributes
         for (t = this.it.nextToken(); 
                 t.getType() != HTMLToken.HTMLTokenType.HT_RIGHT_ANGLE_BRACKET;
                 t = this.it.nextToken()) {
+
             if (t.getType() == HTMLToken.HTMLTokenType.HT_NAME) {
                 String key = t.getValue();
                 String value = new String();
@@ -106,6 +128,10 @@ public class HTMLNoder {
         return ret;
     }
 
+    /**
+     * Parse the next token as text
+     * @return the next node
+     */
     private DOMNode parseText() throws IOException {
         DOMNode ret = new DOMNode();
         ret.type = DOMNode.DOMNodeType.DNT_TEXT;
@@ -115,6 +141,10 @@ public class HTMLNoder {
         return ret;
     }
 
+    /**
+     * Currently skip the next <!> tag
+     * @return the next node
+     */
     private DOMNode parseDefine() throws IOException {
         // TODO parse the define tag
         DOMNode ret = new DOMNode();
@@ -131,6 +161,10 @@ public class HTMLNoder {
         return ret;
     }
 
+    /**
+     * Parse the next token as Comment
+     * @return the next node
+     */
     private DOMNode parseComment() throws IOException {
         DOMNode ret = new DOMNode();
         ret.type = DOMNode.DOMNodeType.DNT_COMMENT;
